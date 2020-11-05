ee7fee86d613be8ba9401ff7f688d234a43bfa1f
EpsIotaPi <1037272365@qq.com>
2020-11-05 11:40:55 +0800

    use best hp

--- HyperParams.py
+++ HyperParams.py
@@ -13,15 +13,15 @@ class HyperParams:
     # 维度，与bert保持一致
     embed_dim = 768
     hidden_size = 768
-    rnn_layers = 3
+    rnn_layers = 5
     '''-------------------调参-------------------'''
     adam_epsilon = 1e-6
     patience = 10
     decay_rate = 0.90
     grad_clip = 3
-    learn_rate = 1e-5
+    learn_rate = 0.00016801566335359662
     min_learn_rate = 8e-6
-    dropout_rate = 0.8
+    dropout_rate = 0.5548877477645874
     '''-------------------调参-------------------'''
 
     bert_path = "./models/chinese_wwm_pytorch"
